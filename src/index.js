96876c6a775d2bba7f36242d0ccbedd54f01eb19
Arnold <arnold.mutama@gmail.com>
2025-04-20 18:04:06 +0300

    Implements a task list

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.getElementById('create-task-form');
+  let taskList = document.getElementById('tasks');
+
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    let taskInput = document.getElementById('new-task-description');
+    let taskText = taskInput.value;
+    let task = document.createElement('li');
+    task.textContent = taskText;
+    taskList.appendChild(task);
+    taskInput.value = '';
+
+  });
+  
 });
